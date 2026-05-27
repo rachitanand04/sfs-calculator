748dd4156575ca6ff69426e0ed39f6efba4b5481
rachitanand04 <rachit.anand04111@outlook.com>
2026-05-27 11:45:16 +0530

    First commit

---
+++ sfs-calc-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="background">
+      <div className="stars"></div>
+      <div className="body">
+        <h1>Spaceflight Simulator</h1>
+        <h2>Flight Parameters Calculator</h2>
+      </div>
+    </div>
+  );
+}
