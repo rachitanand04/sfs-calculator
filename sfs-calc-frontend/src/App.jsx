7ed2ad0aa9779817f6fd7820bd151df789b430a1
rachitanand04 <rachit.anand04111@outlook.com>
2026-06-05 11:21:46 +0530

    Resetting

--- sfs-calc-frontend/src/App.jsx
+++ sfs-calc-frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
 import engines from "./js/engine_info";
-import Calculator from "./components/calculator";
 
 export default function App() {
   return (
-    <div className="background">
-      <div className="stars"></div>
-      <div className="body">
-        <h1>Spaceflight Simulator</h1>
-        <h2>Flight Parameters Calculator</h2>
-        <Calculator />
-      </div>
+    <div>
+      Hello World
     </div>
   );
 }
