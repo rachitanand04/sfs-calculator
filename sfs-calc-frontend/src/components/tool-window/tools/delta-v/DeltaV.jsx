e2e010d2ca9734c6841eb8a0a7b8e9d30a875145
rachitanand04 <rachit.anand04111@outlook.com>
2026-06-08 16:47:08 +0530

    Delta V

--- sfs-calc-frontend/src/components/tool-window/tools/delta-v/DeltaV.jsx
+++ sfs-calc-frontend/src/components/tool-window/tools/delta-v/DeltaV.jsx
@@ -1,25 +1,22 @@
 import "./DeltaV.css";
 import engines from "../../../../js/engine_info.js";
 import gravity from "../../../../js/gravity.js";
-import { calculateBurnTime } from "../../../../js/maths.js";
+import { calculateDeltaV } from "../../../../js/maths.js";
 import { useState } from "react";
 
 export default function DeltaV() {
-  const [mass, setMass] = useState();
-  const [count, setCount] = useState();
+  const [fuelMass, setFuelMass] = useState();
+  const [totalMass, setTotalMass] = useState();
   const [engine, setEngine] = useState("");
-  const [burnTime, setBurnTime] = useState("-");
+  const [deltaV, setDeltaV] = useState("-");
 
   function handleSubmit(e) {
     const engineObject = engines.find((eng) => eng.id == engine);
-    console.log("here");
-    setBurnTime(
-      calculateBurnTime(mass, engineObject.thrust, engineObject.isp, count),
-    );
+    setDeltaV(calculateDeltaV(fuelMass,totalMass,engineObject.isp));
     e.preventDefault();
   }
   return (
-    <div className="burn-time">
+    <div className="delta-v">
       <h2>Delta V</h2>
       <form onSubmit={handleSubmit}>
         <label>Engine</label>
@@ -34,35 +31,27 @@ export default function DeltaV() {
             </option>
           ))}
         </select>
+        <label>Fuel Mass(Tons)</label>
+        <input
+          name="fuelMass"
+          value={fuelMass}
+          type="number"
+          placeholder="Fuel Mass (Tons)"
+          onChange={(e) => setFuelMass(e.target.value)}
+        />
         <label>Total Mass(Tons)</label>
         <input
-          name="mass"
+          name="totalMass"
+          value={totalMass}
           type="number"
-          placeholder="Fuel Mass (Tons)"
-          value={mass}
-          onChange={(e) => setMass(Number(e.target.value))}
-        />
-        <label>Fuel(Tons)</label>
-        <input
-          name="mass"
-          type="number"
-          placeholder="Fuel Mass (Tons)"
-          value={mass}
-          onChange={(e) => setMass(Number(e.target.value))}
-        />
-        <label>Count</label>
-        <input
-          name="count"
-          type="number"
-          placeholder="# of Engines"
-          value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
+          placeholder="Total Mass (Tons)"
+          onChange={(e) => setTotalMass(e.target.value)}
         />
         <button className="pushable" type="submit">
           <span className="front">Go</span>
         </button>
       </form>
-      <div className="burn-time-result">Burn Time: {burnTime} seconds</div>
+      <div className="delta-v-result">Delta V: {deltaV} m/s</div>
     </div>
   );
 }
--- sfs-calc-frontend/src/js/maths.js
+++ sfs-calc-frontend/src/js/maths.js
@@ -1,7 +1,12 @@
 function calculateBurnTime(fuel, thrust, isp, count) {
   const consumptionRate = thrust / isp;
   const burnTime = (fuel / consumptionRate)/count;
-  return (Math.round(burnTime * 100) / 100).toFixed(2);;
+  return (Math.round(burnTime * 100) / 100).toFixed(2);
 }
 
-export {calculateBurnTime};
+function calculateDeltaV(fuelMass, totalMass, isp){
+    const deltaV = isp * 9.80665 * Math.log(totalMass/(totalMass-fuelMass))
+    return (Math.round(deltaV * 100) / 100).toFixed(3);
+}
+
+export {calculateBurnTime, calculateDeltaV};
